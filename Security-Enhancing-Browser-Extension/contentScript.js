0656d84a6f92338b2d50dc270d535b8304ca3171
mitesh_cmd <mitesh.singh@stonybrook.edu>
2019-05-01 06:22:52 -0400

    Added hashing and form detection logic

--- Security-Enhancing-Browser-Extension/contentScript.js
+++ Security-Enhancing-Browser-Extension/contentScript.js
@@ -1,3 +1,43 @@
+
+let FormTypeEnum = {
+	LOGIN: 1,
+	SIGNUP: 2,
+	UNKOWN: 3
+	//Can be extended with RESET form
+}
+
+/*
+Problems with enigma plugin
+
+Does not work in the following cases
+1. Password field is hidden. Ex - Yahoo
+2. Button mentions “Next”/“Continue” - Form Type is Unknown. Ex - Gmail/LinkedIn
+3. Document loads dynamically so difficult to add handler on password input Ex - Instagram
+4. Document has multiple forms. Ex - Facebook
+
+*/
+
+/* Logic to store hash of the password */
+function hexString(buffer) 
+{
+	const byteArray = new Uint8Array(buffer);
+
+  	const hexCodes = [...byteArray].map(value => {
+    const hexCode = value.toString(16);
+    const paddedHexCode = hexCode.padStart(2, '0');
+    	return paddedHexCode;
+  	});
+
+  	return hexCodes.join('');
+}
+
+function getMessageHash(message) 
+{
+  	const encoder = new TextEncoder();
+  	const data = encoder.encode(message);
+  	return window.crypto.subtle.digest('SHA-512', data);
+}
+
 function storeUrlAndPassword(url, password) 
 {
 	chrome.storage.local.get({ enigmaPlugin: []}, function (result) {
@@ -9,10 +49,10 @@ function storeUrlAndPassword(url, password)
 		chrome.storage.local.set({enigmaPlugin: enigmaPlugin}, function () {
 			chrome.storage.local.get('enigmaPlugin', function (result2) {
 
-				//alert(JSON.stringify(result2.enigmaPlugin));
-				document.getElementById("modalHeader").innerHTML = "Password Reuse Warning";
-				document.getElementById("modalBody").innerHTML = "<h2>You reused a password</h2><h2>Please fix</h2><<h2>Please fix</h2>";
-				document.getElementById("myBtn").click();
+				alert(JSON.stringify(result2.enigmaPlugin));
+				//document.getElementById("modalHeader").innerHTML = "Password Reuse Warning";
+				//document.getElementById("modalBody").innerHTML = "<h2>You reused a password</h2><h2>Please fix</h2><<h2>Please fix</h2>";
+				//document.getElementById("myBtn").click();
 
 			});
 		});
@@ -25,70 +65,147 @@ function interceptPassword()
 	if (passwordElem){
 		let url = window.location.host;
 		let password = passwordElem.value;
-		storeUrlAndPassword(url, password);
+
+		getMessageHash(password).then(digestValue => {
+  			let hash = hexString(digestValue);
+  			storeUrlAndPassword(url, hash);
+		});	
 	}
 }
 
-function interceptSubmitEvent() 
+function comparePasswords(result, password, formType)
 {
-	let submitBtn = document.querySelector('input[type=\'submit\'], button[type=\'submit\']');
-	if (submitBtn) {
-		submitBtn.onclick = interceptPassword;
-	}
+	getMessageHash(password).then(digestValue => {
+
+		let hash = hexString(digestValue);
+		let url = window.location.host;
+		let len = result.enigmaPlugin.length;
+		for(let i = 0; i < len; i++) {
+			if(result.enigmaPlugin[i].password === hash && result.enigmaPlugin[i].url != url) {
+
+				if(formType === FormTypeEnum.LOGIN) {
+					alert("You are using password of " +result.enigmaPlugin[i].url+ " on " + url);
+				}
+				else {
+					alert("This is the password of " + result.enigmaPlugin[i].url + " website. Please choose a different password.");
+				}
+				
+			}
+		}
+	});
 }
 
-function comparePasswords(result, password)
+/************** Rules to determine the form type ******************/
+function getFormTypeBySubmitButtonText(formElement)
 {
-	len = result.enigmaPlugin.length;
-	for(i = 0; i < len; i++)
-	{
-		if(result.enigmaPlugin[i].password === password)
-		{
-			alert("SAME PASSWORD USED");
+	let buttonInFormEl = formElement.querySelector('input[type=\'submit\'], button[type=\'submit\']');
+	let text = buttonInFormEl.value + buttonInFormEl.innerText;
+	let logInText = ["log in", "sign in"];
+	for(let i=0; i < logInText.length; i++) {
+		let index = text.toLowerCase().indexOf(logInText[i]);
+		if(index != -1) {
+			return FormTypeEnum.LOGIN;
 		}
 	}
+	
+	let signUpText = ["sign up", "create", "join"];
+	let signUpScore = 0;
+
+	for(let i=0; i < signUpText.length; i++) {
+		let index = text.toLowerCase().indexOf(signUpText[i]);
+		if(index != -1) {
+			return FormTypeEnum.SIGNUP;
+		}
+	}
+
+	return FormTypeEnum.UNKOWN;
+}
+
+function getFormTypeByNoOfPasswordFields(formElement)
+{
+	let passwordElems = formElement.querySelectorAll('input[type=\'password\']');
+	if(passwordElems.length > 1) {
+		return FormTypeEnum.SIGNUP;
+	}
+
+	return FormTypeEnum.UNKOWN;
+}
+
+function getFormTypeByNoOfInputFields(formElement)
+{
+	let inputElems = formElement.querySelectorAll('input[type=\'text\'], input[type=\'email\']');
+	if(inputElems.length > 1) {
+		return FormTypeEnum.SIGNUP;
+	}
+
+	return FormTypeEnum.LOGIN;
+}
+
+// Priority Algorithm
+// The checks for a form being login/signup are written in order
+// If first check satisfies, we don't check the other ones.
+function getFormType(formElement)
+{
+	formType = getFormTypeBySubmitButtonText(formElement);
+	if(formType != FormTypeEnum.UNKOWN)
+		return formType;
+
+	formType = getFormTypeByNoOfPasswordFields(formElement);
+	if(formType != FormTypeEnum.UNKOWN)
+		return formType;
+
+	formType = getFormTypeByNoOfInputFields(formElement);
+	return formType;
 }
 
 
 function interceptUserInput()
 {
 	passwordElem = document.querySelector("input[type=\'password\']");
-	if(passwordElem)
+	if(passwordElem && passwordElem.value) {
+
+		let closestFormElement = passwordElem.closest('form');
+		let formType = getFormType(closestFormElement);
+
 		password = passwordElem.value;
-	chrome.storage.local.get({ enigmaPlugin: []}, function (result) {
-		//check passwords
-		comparePasswords(result, password);
-	});
+		chrome.storage.local.get({ enigmaPlugin: []}, function (result) {
+			comparePasswords(result, password, formType);
+		});
+		
+	}
 }
 
-// chrome.storage.local.clear(function() {
-// });
 
-
-$.get(chrome.extension.getURL("modal.html"), function(data) {
-	$(data).appendTo('body');
-});
-
-passwordElem = document.querySelector("input[type=\'password\']");
-submitBtn = document.querySelector('input[type=\'submit\'], button[type=\'submit\']');
-
+//TODO: Many webpages show the login/signup forms dynamically. The form is not there when page loads.
+//It gets added after some times
+//Ex. Instagram
 window.addEventListener('load',function(){
-	if(submitBtn)
-	{
+
+	let submitBtn = document.querySelector('input[type=\'submit\'], button[type=\'submit\']');
+	if(submitBtn) {
 		submitBtn.onclick = interceptPassword;
 	}
 
-	if(passwordElem) 
-	{
+	let passwordElem = document.querySelector("input[type=\'password\']");
+	if(passwordElem) {
 		passwordElem.oninput = interceptUserInput;
 	}
 });
 
+ //chrome.storage.local.clear(function() {
+ //});
 
 
-/* 
- * Pass message to background.js
- * chrome.runtime.sendMessage(password);
- */
+
+
+//$.get(chrome.extension.getURL("modal.html"), function(data) {
+	//	$(data).appendTo('body');
+//});
+
+
+
+
+
+
 
 
